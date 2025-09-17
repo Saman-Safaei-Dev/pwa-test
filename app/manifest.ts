97f2f2fa77ff28ba2ac24737fe3f4b4701649aaf
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-09-17 21:18:33 +0330

    chore: replace next example manifest with actual values

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,21 +2,22 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js PWA",
-    short_name: "NextPWA",
-    description: "A Progressive Web App built with Next.js",
     start_url: "/",
+    short_name: "PWAM",
+    name: "PWA Messenger",
     display: "standalone",
+    theme_color: "#ffffff",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    description:
+      "A Progressive Web App built with Next.js & Developed by the Saman-Safaei-Dev",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
